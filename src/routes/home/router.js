6e5079210dce07219f1fa2e43f77386b4bdbfd78
zzzhangling <404403688@qq.com>
2020-03-30 16:11:53 +0800

    change innner route

--- src/routes/home/router.js
+++ src/routes/home/router.js
@@ -2,12 +2,12 @@
  * @Author: zhangLing
  * @Date: 2020-03-23 13:56:42
  * @LastEditors: zhangLing
- * @LastEditTime: 2020-03-24 14:58:55
+ * @LastEditTime: 2020-03-26 16:23:43
  * @Description: 文件描述
  */
 import Home from './index'
 
-export default {
-  path: '/home',
+export default [{
+  path: '/',
   component: Home,
-}
+}]
