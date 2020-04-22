dfc43dfe515151e93cdfde8058063c6b60f5f978
zzzhangling <404403688@qq.com>
2020-04-22 11:46:34 +0800

    update file

--- src/routes/center/details.js
+++ src/routes/center/details.js
@@ -2,7 +2,7 @@
  * @Author: zhangLing
  * @Date: 2020-03-24 15:12:53
  * @LastEditors: zhangLing
- * @LastEditTime: 2020-03-24 15:15:01
+ * @LastEditTime: 2020-04-14 17:10:05
  * @Description: 文件描述
  */
 import React from 'react'
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -2,17 +2,19 @@
  * @Author: zhangLing
  * @Date: 2020-03-20 14:41:38
  * @LastEditors: zhangLing
- * @LastEditTime: 2020-04-01 16:12:01
+ * @LastEditTime: 2020-04-14 17:11:04
  * @Description: 文件描述
  */
-import React, { useState } from "react";
+import React, { useState, useCallback, } from "react";
 
 const Home = props => {
   const [num, setNum] = useState(0);
-  const plusNum = () => {
-    setNum(num + 1)
-  }
-  console.log('render')
+  const plusNum = useCallback(
+    () => {
+      setNum(num + 1)
+    },
+    [num],
+  )
   return (
     <div>
       <h1>Home</h1>
